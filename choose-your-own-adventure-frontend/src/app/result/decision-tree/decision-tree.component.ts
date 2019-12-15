5917b83edbf3560af081787a481f23443c7c078c
Dmytro Hrynevych <grinevich97@gmail.com>
2019-12-15 15:36:21 +0200

    Add orgChart package

--- choose-your-own-adventure-frontend/src/app/result/decision-tree/decision-tree.component.ts
+++ choose-your-own-adventure-frontend/src/app/result/decision-tree/decision-tree.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-decision-tree',
   templateUrl: './decision-tree.component.html',
@@ -10,6 +12,30 @@ export class DecisionTreeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const datasource = {
+      name: 'Lao Lao',
+      title: 'general manager',
+      children: [
+        { name: 'Bo Miao', title: 'department manager' },
+        { name: 'Su Miao', title: 'department manager',
+          children: [
+            { name: 'Tie Hua', title: 'senior engineer' },
+            { name: 'Hei Hei', title: 'senior engineer',
+              children: [
+                { name: 'Dan Dan', title: 'engineer' }
+              ]
+            },
+            { name: 'Pang Pang', title: 'senior engineer' }
+          ]
+        },
+        { name: 'Hong Miao', title: 'department manager' }
+      ]
+    };
+    $('#chart-container').orgchart({
+      data : datasource,
+      nodeContent: 'title'
+    });
   }
 
 }
