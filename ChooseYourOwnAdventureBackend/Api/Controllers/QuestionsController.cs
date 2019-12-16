f146f4ad07ee4b5752940b6d6ba47855d3d0afcc
Dmitry Grinevich <dmitry.grinevich@idealscorp.com>
2019-12-16 17:50:25 +0200

    Create questions controller

---
+++ ChooseYourOwnAdventureBackend/Api/Controllers/QuestionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("questions")]
+    public class QuestionsController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public IActionResult GetQuestion(int id)
+        {
+            return Ok();
+        }
+
+        [HttpGet("tree")]
+        public IActionResult GetQuestionsTree()
+        {
+            return Ok();
+        }
+    }
+}
