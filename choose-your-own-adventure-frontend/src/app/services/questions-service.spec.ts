1b56ad5e6d0e87b59e13089b2254d33078fa9cd6
Dmytro Hrynevych <grinevich97@gmail.com>
2019-12-17 23:20:40 +0200

    Start making tests green

--- choose-your-own-adventure-frontend/src/app/services/questions-service.spec.ts
+++ choose-your-own-adventure-frontend/src/app/services/questions-service.spec.ts
@@ -1,7 +1,23 @@
 import { QuestionsService } from './questions-service';
+import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('QuestionsService', () => {
+  let service: QuestionsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        QuestionsService,
+        {provider: 'API_IRL', useValue: 'test url'}
+      ],
+      imports: [HttpClientTestingModule]
+    });
+
+    service = TestBed.get(QuestionsService);
+  });
+
   it('should create an instance', () => {
-    expect(new QuestionsService()).toBeTruthy();
+    expect(service).toBeTruthy();
   });
 });
