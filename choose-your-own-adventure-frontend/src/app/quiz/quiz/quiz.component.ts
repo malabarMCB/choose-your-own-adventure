f049f5193c0bf7d8068798d80cdf140b42c9fec4
Dmytro Hrynevych <grinevich97@gmail.com>
2019-12-17 20:59:35 +0200

    Add navigation according to quiz state

--- choose-your-own-adventure-frontend/src/app/quiz/quiz/quiz.component.ts
+++ choose-your-own-adventure-frontend/src/app/quiz/quiz/quiz.component.ts
@@ -1,25 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {QuestionsService} from '../../services/questions-service';
 import {Question} from '../../models/question';
-import {AppState} from '../../store/app-store.reducer';
+import {AppState, isQuizFinished} from '../../store/app-store.reducer';
 import {Store} from '@ngrx/store';
 import * as Actions from '../../store/app-store.actions';
 import {Router} from '@angular/router';
 import {QuestionAnswer} from '../../models/question-answer.enum';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent implements OnInit {
+export class QuizComponent implements OnInit, OnDestroy {
+
   currentQuestion: Question;
+  isQuizFinishedSubscription: Subscription;
 
   constructor(private readonly questionService: QuestionsService,
               private readonly store: Store<AppState>,
               private readonly router: Router) { }
 
   ngOnInit() {
+    this.isQuizFinishedSubscription = this.store.select(isQuizFinished).subscribe(isFinished => {
+      if (isFinished) {
+        this.router.navigate(['result']);
+      }
+    });
+
     this.setCurrentQuestion(1);
   }
 
@@ -36,13 +45,15 @@ export class QuizComponent implements OnInit {
   private setCurrentQuestion(id: number) {
     this.questionService.getQuestion(id).subscribe(question => {
       if (!question.negativeAnswerQuestionId && !question.positiveAnswerQuestionId) {
-
-        this.store.dispatch(Actions.setQuizStatus({isFinished: true}));
         this.store.dispatch(Actions.addAnsweredQuestion({question}));
-
-        this.router.navigate(['result']);
+        this.store.dispatch(Actions.setQuizStatus({isFinished: true}));
       }
+
       this.currentQuestion = question;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.isQuizFinishedSubscription.unsubscribe();
   }
 }
--- choose-your-own-adventure-frontend/src/app/result/result/result.component.ts
+++ choose-your-own-adventure-frontend/src/app/result/result/result.component.ts
@@ -1,23 +1,25 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {QuestionsService} from '../../services/questions-service';
 import {QuestionTreeNode} from '../../models/question-tree-node';
 import {DecisionTreeComponent} from '../decision-tree/decision-tree.component';
 import {Store} from '@ngrx/store';
-import {AppState, getAnsweredQuestionsIds} from '../../store/app-store.reducer';
+import {AppState, getAnsweredQuestionsIds, isQuizFinished} from '../../store/app-store.reducer';
 import {first} from 'rxjs/operators';
 import * as Actions from '../../store/app-store.actions';
 import {Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss']
 })
-export class ResultComponent implements OnInit {
+export class ResultComponent implements OnInit, OnDestroy {
   @ViewChild(DecisionTreeComponent, {static: false}) decisionTree: DecisionTreeComponent;
 
   questionsTree: QuestionTreeNode;
   answeredQuestionsIds: number[];
+  isQuizFinishedSubscription: Subscription;
 
   constructor(private readonly questionsService: QuestionsService,
               private readonly store: Store<AppState>,
@@ -26,6 +28,12 @@ export class ResultComponent implements OnInit {
   ngOnInit() {
     this.questionsService.getAllQuestions().subscribe(questions => this.questionsTree = questions);
     this.store.select(getAnsweredQuestionsIds).pipe(first()).subscribe(ids => this.answeredQuestionsIds = ids);
+
+    this.isQuizFinishedSubscription = this.store.select(isQuizFinished).subscribe(isFinished => {
+      if (!isFinished) {
+        this.router.navigate(['']);
+      }
+    });
   }
 
   highlightAnsweredQuestions(): boolean {
@@ -34,9 +42,12 @@ export class ResultComponent implements OnInit {
   }
 
   playAgain(): boolean {
-    this.store.dispatch(Actions.setQuizStatus({isFinished: false}));
     this.store.dispatch(Actions.clearAnsweredQuestions());
-    this.router.navigate(['']);
+    this.store.dispatch(Actions.setQuizStatus({isFinished: false}));
     return false;
+  }
+
+  ngOnDestroy(): void {
+    this.isQuizFinishedSubscription.unsubscribe();
   }
 }
